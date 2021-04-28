782f7d0b7cb5e390272368cd9eff18a3b26ff03b
swordysrepo <81474566+swordysrepo@users.noreply.github.com>
2021-04-28 12:58:01 -0400

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -5,7 +5,7 @@ Created on Sat Apr 24 21:00:04 2021
 @author: swordysrepo
 """
 
-REFRESH_TIME = 120 #seconds
+REFRESH_TIME = 300 #seconds
 STOCKS_TO_CHECK = [
 "DOGE-USD",  
 # "",
@@ -17,7 +17,24 @@ STOCKS_TO_CHECK = [
 
 PERCENTAGE_TO_ALERT = 3 #percent
 SOUND_TO_PLAY = "" # which MP4 we want to play when something happens
+RELEVANT_WEBSITES = [
+"",
+"",
+"",
 
+    
+    
+    ]
+
+SMS_NUMBERS = [
+"",
+"",
+"",
+    
+    
+    ]
+
+DB_NAME = "stocks"
 
 
 
